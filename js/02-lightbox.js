f243832c389de6d5ae26d398cd400b2d780b2b54
Magdalena Zawisza <zawisza.magdalena@gmail.com>
2023-03-30 14:02:30 +0200

    Zadanie 1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map((item) => {
+    return `
+    <a class="gallery__item" href="${item.original}">
+    <img 
+    class="gallery__image" 
+    src="${item.preview}" 
+    alt="${item.description}" 
+    />
+  </a>`;
+  })
+  .join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const imageSource = e.target.dataset.source;
+});
+
